d949115b7b374080fb1e6cc31335e56533f42700
Velibor Markovic <veljko85@users.noreply.github.com>
2017-06-13 21:13:59 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -7,5 +7,23 @@ $(document).ready(function(){
 		$(".line3").toggleClass("ukrstanje-suprotno");
 
 		$(".navigationBar").slideToggle();
+
+
 	});
+	
+	$(".navigationBar a").click( function() {
+		
+		$(".newsSectionHome").hide();
+		$(".section").show();
+
+		$(".line2").fadeToggle();
+		$(".line1").toggleClass("ukrstanje");
+		$(".line3").toggleClass("ukrstanje-suprotno");
+		$(".navigationBar").slideToggle();
+		
+		$(".section").load($(this).attr("href")).hide().fadeIn();
+        	return(false);
+	});
+
+
 });
